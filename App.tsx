df5b4973bde4a68a25a0565cb53d35d851f53939
hsinha1110 <himanshusinha1110@gmail.com>
2026-01-30 04:23:49 +0530

    .env

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, ActivityIndicator,   } from "react-native";
+import {API_HOST} from '@env';
+import { User } from "./src/types/types";
+import {SafeAreaView} from 'react-native-safe-area-context'
+
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+  const [data, setData] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const apiURL=`${API_HOST}/users`
 
-export default App
+  useEffect(() => {
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(json => setData(json))
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" />
+  }
+
+  return (
+    <SafeAreaView style={{padding:20}}>
+      <FlatList<User>
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text>{item.name}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
